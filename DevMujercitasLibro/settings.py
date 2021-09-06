3e4126eff6ab23a163a294a2131e7d5b6106568e
katherin <katheringabriela2310@gmail.com>
2021-09-05 20:32:42 -0600

    deploy3

--- DevMujercitasLibro/settings.py
+++ DevMujercitasLibro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c(@!b3qc%^gr(bc#8&cc^y%q2qc0!f2*fhbp+a4aq_-ln!v$s5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'DevMujercitasLibro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,7 +92,7 @@ DATABASES = {
     )
 }
 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
